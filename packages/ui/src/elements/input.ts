f8314479d0379ccef9354d3852dee7a8f8850332
dannysmc95 <danny.smc95@gmail.com>
2025-09-18 09:41:39 +0000

    Updated dependencies

--- packages/ui/src/elements/input.ts
+++
@@ -1,7 +0,0 @@
-import BaseElement from './base';
-
-export default class Input extends BaseElement {
-	public onChange(callback: (value: string) => void) {
-		console.log(callback);
-	}
-}
--- packages/ui/src/elements/text.ts
+++
@@ -1,5 +0,0 @@
-import BaseElement from './base';
-
-export default class Text extends BaseElement {
-
-}

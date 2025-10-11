56504aaceb765eff376924317e92b3a7a8049e0c
dannysmc95 <danny.smc95@gmail.com>
2025-10-11 02:16:23 +0000

    Fixed some issues within existing packages.

--- packages/http/src/module/plugin.ts
+++ packages/http/src/module/plugin.ts
@@ -5,23 +5,10 @@ import SseConnectionsProvider from '../provider/sse-connections';
 import HttpService from '../service/http';
 import CorsMiddleware from '../middlewares/cors';
 
-/**
- * Initialises the package file and pulls the data from that
- * ready for the plugin to use.
- */
 const packageJson = file(Utils.resolve(import.meta.dirname, '../../package.json'));
 if (!await packageJson.exists()) throw new Error('Package.json not found.');
 const packageMeta = await packageJson.json();
 
-/**
- * The HTTP plugin for Sodacore, this will provide the HTTP service
- * and the controller decorators for building a HTTP powered RESTful
- * API with ease.
- * @class HttpPlugin
- * @implements {IPlugin}
- * @extends {BasePlugin}
- * @default
- */
 export default class HttpPlugin extends BasePlugin implements IPlugin {
 	public name = packageMeta.name;
 	public version = packageMeta.version;
@@ -29,23 +16,11 @@ export default class HttpPlugin extends BasePlugin implements IPlugin {
 	public author = packageMeta.author;
 	public dependencies = [];
 
-	/**
-	 * Will initialise the HttpPlugin, called usually by the autowire
-	 * module to setup.
-	 * @param config The configuration for the plugin.
-	 * @constructor
-	 */
 	public constructor(protected config: IConfig = { port: 8080 }) {
 		super(config);
 		this.setup();
 	}
 
-	/**
-	 * Will be called by the autowire module of the sodacore
-	 * core application to initialise the given modules.
-	 * @param app Application
-	 * @returns void
-	 */
 	public async install(app: Application) {
 		app.register(SseConnectionsProvider);
 		app.register(HttpService);
